c30c69fe4e402c4127f5b7bbdd0319b817bacfd3
vasuvemu <vasuvemu@gmail.com>
2025-04-07 22:45:17 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,8 @@ class HelloWorld
 {
     public static void main(String args[])
 	{
-	     System.out.println("Hello world.....>");
-		 System.out.println("Hello world git hub ..AWS");
-		 System.out.println("Hello world first java progran.........test");
+	     System.out.println("Hello world.....> po1");
+		 System.out.println("Hello world git hub ..AWSpo2");
+		 System.out.println("Hello world first java progran.........test, p3");
 	}
 }
